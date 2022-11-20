87e43f130f307776ccfb8ce341e33573e3ba0474
mehrdad <=>
2022-11-20 10:52:58 +0330

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# friends
+a fun mini-project that will make my time waste :D 
+
+it use's bs4 and requests libraries to get all episodes of 10 seasons of the friends series  
---
+++ friends.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+base = 'https://sv.dcnm.ir/Serial/Foreign/'
+
+for season in range(1,11):
+    url = f'https://sv.dcnm.ir/Serial/Foreign/?dir=Friends/720/S0{season}'
+    page = requests.get(url).text
+    soup = BeautifulSoup(page,features="html5lib")
+    links = soup.find_all('a',class_='clearfix')
+    for link in links[1:] :
+        print(f"{base}{link['href']}")
+    
