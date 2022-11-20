0b7e96547c9f6cafcb46f707bbb0c8d334b5231e
mehrdad <=>
2022-11-20 12:29:55 +0330

    now insted of just printing the link to console it will create a file based of the seasons

--- friends.py
+++ friends.py
@@ -3,10 +3,12 @@ import requests
 base = 'https://sv.dcnm.ir/Serial/Foreign/'
 
 for season in range(1,11):
+    f = open(f'season{season}',"w")
     url = f'https://sv.dcnm.ir/Serial/Foreign/?dir=Friends/720/S0{season}'
     page = requests.get(url).text
     soup = BeautifulSoup(page,features="html5lib")
     links = soup.find_all('a',class_='clearfix')
     for link in links[1:] :
-        print(f"{base}{link['href']}")
-    
+        print(f"{base}{link['href']}\n")
+        f.write(f"{base}{link['href']}\n")
+    f.close()
